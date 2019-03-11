ba79523c3ddc63f5a2c8d9ab538441cb17b13c63
gecastro <gecastro@expedia.com>
2019-03-11 11:32:53 -0600

    Add build to server

---
+++ public/build/precache-manifest.5d9ed141b3542ad3bc4ce28bac743091.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fe278881562e8396f91",
+    "url": "/static/css/main.bad791d5.chunk.css"
+  },
+  {
+    "revision": "8fe278881562e8396f91",
+    "url": "/static/js/main.8fe27888.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9f3b284ddc4409314533",
+    "url": "/static/js/2.9f3b284d.chunk.js"
+  },
+  {
+    "revision": "b84395e08854aadfd9c07790a6aa6426",
+    "url": "/index.html"
+  }
+];
